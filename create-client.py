e3acf1f6565b1db71c74ec9f095c61a7502ed1a7
sabrinaspage <sabreyes01@gmail.com>
2021-01-12 19:56:23 -0500

    plug info and rename

--- create-client.py
+++ create-client.py
@@ -6,6 +6,6 @@ PORT = 65432 # The port used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'hello,world') # bytes object
-    data = s.recv(1024)
+    data = s.recv(1024) # receive the data sent to the server
 
 print('Received', repr(data))
