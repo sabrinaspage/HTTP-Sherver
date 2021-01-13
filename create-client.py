9589324e16a628779e7d3813c8506d00ad434f7e
sabrinaspage <sabreyes01@gmail.com>
2021-01-12 22:31:55 -0500

    GET, SET

--- create-client.py
+++ create-client.py
@@ -5,7 +5,7 @@ PORT = 65432 # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'GET sidestep') # bytes object
+    s.sendall(b'SET banana-curved, yellow fruit with thick skin and soft sweet flesh') # bytes object
     data = s.recv(1024) # receive the response from the server
 
 print('Received', repr(data))
--- create-server-dict.py
+++ create-server-dict.py
@@ -12,6 +12,24 @@ dictionary = {
 HOST = '127.0.0.1' # Standard loopback interface address (localhost)
 PORT = 65432 # Port to listen on (non-privileged ports are > 1023)
 
+def getWord(data):
+    data = data.partition("GET" + " ")[2]
+    if data in dictionary.keys():
+         data = 'ANSWER ' + dictionary.get(data)
+    else:
+        data = 'ERROR ' + data + ' does not exist'
+    return data
+
+def setDefinition(data):
+    data = data.partition("SET" + " ")[2]
+    splitData = data.split('-')
+    key = splitData[0]
+    value = splitData[1]
+
+    dictionary[key] = value
+    return key + ' has been set'
+    
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -22,13 +40,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while True: # infinite loop
             data = conn.recv(1024)
             dataReq = data.decode() # turns to string
-            dataReq = dataReq.partition("GET" + " ")[2]
 
-            if dataReq in dictionary.keys():
-                dataReq = 'ANSWER ' + dictionary.get(dataReq)
-            else:
-                dataReq = 'ERROR ' + dataReq + ' does not exist'
+            if "GET" in dataReq:
+                dataReq = getWord(dataReq)
+
+            if "SET" in dataReq:
+                dataReq = setDefinition(dataReq)
+
             if not data:
                 break
+
             dataReq = str.encode(dataReq) # turns back to bytes
             conn.sendall(dataReq)
